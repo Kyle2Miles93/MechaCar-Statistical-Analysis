413517648861818f58263de045f9e4cb5c5454a2
Miles Tsue <65318978+Kyle2Miles93@users.noreply.github.com>
2021-03-26 15:44:14 -0400

    modified script for T-Tests.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,12 @@ library(tidyverse)
 
 total_summary_df <- Sus_Coil_df %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups="keep")
 lot_summary_df <- Sus_Coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+
+
+t.test(Sus_Coil_df$PSI, mu=1500)
+
+t.test(subset(Sus_Coil_df, Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+t.test(subset(Sus_Coil_df, Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+t.test(subset(Sus_Coil_df, Manufacturing_Lot=="Lot3")$PSI, mu=1500)
+
+
